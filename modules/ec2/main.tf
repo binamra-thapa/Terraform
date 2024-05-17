a3b277274911e2f1c3bfd4e18181dc3733164a82
Binamra.Thapa <binamra.thapa@bottomline.com>
2024-05-17 21:49:58 +0530

    ADding modules

---
+++ modules/ec2/main.tf
@@ -0,0 +1,10 @@
+# main.tf
+resource "aws_instance" "instance" {
+  ami                       = var.ami_id
+  instance_type             = var.instance_type
+  key_name                  = var.key_name
+  security_groups           = var.firewall_group
+  tags          = {
+    instance_name = var.instance_name
+  }
+}
