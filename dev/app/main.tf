db64a04c8303c791a915343f8fce1e222a45aaae
Binamra.Thapa <binamra.thapa@bottomline.com>
2024-05-24 14:38:08 +0530

    updating the module name

--- dev/app/main.tf
+++ dev/app/main.tf
@@ -6,16 +6,14 @@ terraform {
     }
   }
 }
-provider "aws" {
-  region = "ap-south-1"
-}
+
 
 module "bt_app_01" {
   source         = "../../modules/ec2"
-  ami_id         = "ami-04708942c263d8190"  
-  instance_type  = "t2.micro"      
+  ami_id         = "ami-04708942c263d8190"
+  instance_type  = "t2.micro"
   key_name       = "key-value"
-  firewall_group = ["SSH GROUP"] 
+  firewall_group = ["SSH GROUP"]
   hostname       = "binnie"
   device_name    = "Testing"
 
@@ -24,8 +22,8 @@ module "bt_app_01" {
 #output
 output "bt_app_01" {
   value = {
-    instance_id   = module.bt_app_01.instance_id
-    public_ip     = module.bt_app_01.public_ip
-    private_ip    = module.bt_app_01.private_ip
+    instance_id = module.bt_app_01.instance_id
+    public_ip   = module.bt_app_01.public_ip
+    private_ip  = module.bt_app_01.private_ip
   }
 }
